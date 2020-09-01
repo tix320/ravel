9cff3e2baa4e1ff673f8e093cc19ff90655cf103
Tigran Sargsyan <tigran.sargsyan@outlook.com>
2020-09-01 09:45:23 +0400

    Export api packages.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
 module com.github.tix.ravel {
 	exports com.github.tix320.ravel.api;
+	exports com.github.tix320.ravel.api.bean;
+	exports com.github.tix320.ravel.api.module;
+	exports com.github.tix320.ravel.api.scope;
 }
